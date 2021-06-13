4a9d58f5f29cc935511bcfade54f760aacdac067
Nguyen Tuan Minh <minhnt10@topica.edu.vn>
2021-06-13 20:18:09 +0700

    update folder

--- component/model.py
+++ component/model.py
@@ -5,7 +5,7 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from component.muscular_degradation import get_muscular_degradation_model
 from component.respiratory_tract import get_respiratory_tract_model
-from component.sentiment import  get_sentiment_model
+from component.sentiment import get_sentiment_model
 from component.vocal_cords import get_vocal_cords_model
 
 
@@ -16,12 +16,22 @@ class CovidClassifier(keras.Model, ABC):
         self.biomaker3 = get_sentiment_model()
         self.biomaker4 = get_respiratory_tract_model()
 
+    def load_weights(self,
+                     muscular_path,
+                     vocal_cords_path,
+                     sentiment_path,
+                     tract_path):
+        self.biomaker1.load_weights(muscular_path)
+        self.biomaker2.load_weights(vocal_cords_path)
+        self.biomaker3.load_weights(sentiment_path)
+        self.biomaker4.load_weights(tract_path)
+
     def call(self, inputs):
         x = self.biomaker1(inputs)
         v = self.biomaker2(x)
-        l = self.biomaker3(x)
-        s = self.biomaker4(x)
-        concat = layers.concatenate([v, l, s], axis=-1)
+        s = self.biomaker3(x)
+        r = self.biomaker4(x)
+        concat = layers.concatenate([v, s, r], axis=-1)
         x = tf.keras.layers.GlobalAveragePooling2D()(concat)
         x = layers.Dense(1024, activation='relu')(x)
         x = layers.Dense(1, activation=tf.nn.sigmoid)(x)
