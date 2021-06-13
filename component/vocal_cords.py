a6703f14f511f6ee8c5ed67a4624557b08764bb5
Nguyen Tuan Minh <minhnt10@topica.edu.vn>
2021-06-13 19:58:06 +0700

    add pretrained weights

--- component/vocal_cords.py
+++ component/vocal_cords.py
@@ -6,7 +6,16 @@ from tensorflow.keras import layers
 from tensorflow.keras.applications import resnet50
 
 
-def get_vocal_cords_model(input_shape=(300, 200)):
-    model = resnet50.ResNet50(include_top=False, input_shape=input_shape)
+def get_vocal_cords_model(input_shape, weights=None):
+    base_model = resnet50.ResNet50(input_shape=input_shape, include_top=False, weights=weights)
+    x1 = keras.layers.GlobalAveragePooling2D()(base_model.output)
+    x2 = keras.layers.GlobalMaxPooling2D()(base_model.output)
+    x = keras.layers.concatenate([x1,x2])
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.Dropout(0.7)(x)
+    x = keras.layers.Dense(1024, activation='relu')(x)
+    x = keras.layers.BatchNormalization()(x)
+    x = keras.layers.Dropout(0.7)(x)
+    output = keras.layers.Dense(1, activation='sigmoid')(x)
+    model = keras.models.Model(inputs=[base_model.input], outputs=[output])
     return model
-
